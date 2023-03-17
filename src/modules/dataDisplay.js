55b7c205737207f0d628d4dc3ec39585954ea07d
Rana-Mudassir <mduasire38@gmail.com>
2023-03-18 02:15:48 +0500

    Update linter error

--- src/modules/dataDisplay.js
+++ src/modules/dataDisplay.js
@@ -1,11 +1,11 @@
-const htmlInput = (arr) => {
-  arr.forEach((data) => {
-    document.querySelector('.table-form').innerHTML += `
-        <tr>
-            <td>${data.name}: ${data.score}</td>
-        </tr>
-      `;
-  });
-};
+// const htmlInput = (arr) => {
+//   arr.forEach((data) => {
+//     document.querySelector('.table-form').innerHTML += `
+//         <tr>
+//             <td>${data.name}: ${data.score}</td>
+//         </tr>
+//       `;
+//   });
+// };
 
-export default htmlInput;
+// export default htmlInput;
