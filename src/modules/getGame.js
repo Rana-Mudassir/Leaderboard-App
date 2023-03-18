580ab256cb80bb873514300bb8d50b38e98ca91b
Rana-Mudassir <mduasire38@gmail.com>
2023-03-19 02:49:35 +0500

    update linters

--- src/modules/getGame.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable linebreak-style */
-const getScores = async (url) => fetch(url, {
-  method: 'GET',
-  headers: { 'Content-Type': 'application/json' },
-})
-  .then((response) => response.json())
-  .then((response) => response.result);
-
-export default getScores;
